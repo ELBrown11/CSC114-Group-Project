ca49a2b03a1ba41f67de2fde3aa3510834214f10
Eboni <34903672+ELBrown11@users.noreply.github.com>
2021-04-08 18:22:53 -0400

    Menu Options

---
+++ assignment8_Eboni.cpp
@@ -0,0 +1,23 @@
+//displaymenu function
+
+#include <iostream>
+using namespace std;
+
+//function prototype
+void DisplayMenu()
+
+//***********************************************************
+// Defintion of function DisplayMenu shows the menu options *
+//***********************************************************
+
+void DisplayMenu()
+{
+    cout << "\nTheather Options Menu\n\n"
+         << "1. Purchase Tickets\n"
+         << "2. Total Ticket Sales in Dollars\n"
+         << "3. Total Number of Tickets Sold\n"
+         << "4. Seat Availability in Each Row\n"
+         << "5. Seat Availability in Auditorium\n"
+         << "6. Exit\n"
+         << "Enter your choice: ";
+}
