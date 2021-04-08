ee66f8079392287eac48b12b12647bfa23d1ee7f
Eboni <34903672+ELBrown11@users.noreply.github.com>
2021-04-08 18:46:14 -0400

    Moved my function definition to the bottom

--- assignment8_Eboni.cpp
+++ assignment8_Eboni.cpp
@@ -1,30 +1,13 @@
-//displaymenu function
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //function prototype
 void DisplayMenu()
 
-//***********************************************************
-// Defintion of function DisplayMenu shows the menu options *
-//***********************************************************
 
-void DisplayMenu()
-{
-    cout << "\nTheather Options Menu\n\n"
-         << "1. Purchase Tickets\n"
-         << "2. Total Ticket Sales in Dollars\n"
-         << "3. Total Number of Tickets Sold\n"
-         << "4. Seat Availability in Each Row\n"
-         << "5. Seat Availability in Auditorium\n"
-         << "6. Exit\n"
-         << "Enter your choice: ";
-}
 
-#include <iostream>
-#include <iomanip>
-using namespace std;
 
 
 const int x = 30;           //Number of columns used for seating chart array
@@ -70,3 +53,18 @@ void displayChart(){
 	}
 }
 
+//***********************************************************
+// Defintion of function DisplayMenu shows the menu options *
+//***********************************************************
+
+void DisplayMenu()
+{
+    cout << "\nTheather Options Menu\n\n"
+         << "1. Purchase Tickets\n"
+         << "2. Total Ticket Sales in Dollars\n"
+         << "3. Total Number of Tickets Sold\n"
+         << "4. Seat Availability in Each Row\n"
+         << "5. Seat Availability in Auditorium\n"
+         << "6. Exit\n"
+         << "Enter your choice: ";
+}
